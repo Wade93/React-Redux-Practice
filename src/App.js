9ea332bdd965ef4a67a5e7d17c667dcf51f932ab
Wade Allen <wade.allen11@gmail.com>
2019-11-12 19:45:34 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Posts from './components/Posts';
+import PostForm from './components/Postform'
+
+function App() {
+  return (
+    <div className="App">
+      <PostForm/>
+      <hr></hr>
+      <Posts/>
+    </div>
+  );
+}
+
+export default App;
